1c0be6b6a86dea6921ddcf837bc1d22e6fa30b91
kiankwa <kian.kwa@justice.gov.uk>
2022-02-01 11:33:53 +0000

    Add logging to test slack bot in live

--- src/service/helpRequestManager.js
+++ src/service/helpRequestManager.js
@@ -25,7 +25,40 @@ async function handleSupportRequest(client, user, helpRequest) {
     })
 }
 
+function checkConfigPresent(config, output) {
+    if (config !== undefined || config !== null || config !== "" ) {
+        console.log("*****" + output)
+        return true
+    }
+    return false
+}
+
 async function handleBugReport(client, user, helpRequest) {
+    console.log("*****TESTING INPUT PARAMETERS")
+    if (client.token === undefined || client.token === null || !client.token.startsWith("xoxb")) {
+        console.log("*****Invalid channel bot token in Client")
+    } else {
+        console.log("*****Channel token present in Client")
+    }
+
+    console.log("*****Slack user is: " + user)
+
+    console.log("*****TESTING CONFIG SETTINGS")
+    const channelPresent = checkConfigPresent(config.get('slack.report_channel'), "Slack channel config present")
+    if (channelPresent) {
+        console.log("*****Slack channel value: " + config.get('slack.report_channel'))
+    }
+
+    const channelIdPresent = checkConfigPresent(config.get('slack.report_channel_id'), "Slack channel id config present")
+    if (channelIdPresent) {
+        console.log("*****Slack channel id value: " + config.get('slack.report_channel_id'))
+    }
+
+    checkConfigPresent(config.get('secrets.cftptl-intsvc.jira-username'), "Jira username config present")
+    checkConfigPresent(config.get('secrets.cftptl-intsvc.jira-api-token'), "Jira api token config present")
+    checkConfigPresent(config.get('secrets.cftptl-intsvc.idam-slack-bot-token'), "Idam slack bot token config present")
+    checkConfigPresent(config.get('secrets.cftptl-intsvc.idam-slack-app-token'), "Idam slack app token config present")
+
     const userEmail = await getUserEmail(client, user)
     const jiraId = await createHelpRequest(helpRequest, userEmail, JiraType.BUG.id)
 
